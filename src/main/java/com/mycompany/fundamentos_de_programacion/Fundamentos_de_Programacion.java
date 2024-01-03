d60d7d86ac4a46ffd814a282db7612c2cdb9c48a
Dante Silva <dantesilvacalderon117@gmail.com>
2024-01-02 22:26:01 -0600

    first commit

---
+++ src/main/java/com/mycompany/fundamentos_de_programacion/Fundamentos_de_Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fundamentos_de_programacion;
+
+/**
+ *
+ * @author dante
+ */
+public class Fundamentos_de_Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
